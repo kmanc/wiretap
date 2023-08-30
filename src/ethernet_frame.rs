d7b70458afdfa210ca0b825eea7f3e9c6bd300a8
koins <koins@duck.com>
2023-08-29 20:29:15 -0700

    split things into modules for easier maintenance

---
+++ src/ethernet_frame.rs
@@ -0,0 +1,46 @@
+use pnet::packet::Packet;
+use pnet::packet::ethernet::EthernetPacket as pnet_EthernetPacket;
+use std::ops::Deref;
+use std::sync::Arc;
+
+/// Wrapper around pnet's EthernetPacket for adding additional funcitonality
+#[derive(Debug)]
+pub struct EthernetFrame<'a>(pnet_EthernetPacket<'a>);
+
+impl<'a> From<pnet_EthernetPacket<'a>> for EthernetFrame<'a> {
+    fn from(ethernet_frame: pnet_EthernetPacket<'a>) -> Self {
+        EthernetFrame(ethernet_frame)
+    }
+}
+
+impl<'a> Deref for EthernetFrame<'a> {
+    type Target = pnet_EthernetPacket<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl EthernetFrame<'_> {
+    pub fn create_clone<'a>(&self) -> EthernetFrame<'a> {
+        EthernetFrame::from(pnet_EthernetPacket::owned(self.packet().to_vec()).unwrap())
+    }
+}
+
+/// Wrapper around an Arc<[EthernetFrame]> for additional functionality
+#[derive(Debug)]
+pub struct EthernetFrameCollection<'a>(Arc<[EthernetFrame<'a>]>);
+
+impl<'a> FromIterator<EthernetFrame<'a>> for EthernetFrameCollection<'a> {
+    fn from_iter<I: IntoIterator<Item = EthernetFrame<'a>>>(iter: I) -> Self {
+        EthernetFrameCollection(iter.into_iter().collect())
+    }
+}
+
+impl<'a> Deref for EthernetFrameCollection<'a> {
+    type Target = Arc<[EthernetFrame<'a>]>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
---
+++ src/tcp_packet.rs
@@ -0,0 +1,164 @@
+use pnet::packet::Packet;
+use pnet::packet::tcp::TcpPacket as pnet_TcpPacket;
+use std::ops::{Deref, DerefMut};
+use std::sync::Arc;
+
+/// Wrapper around pnet's TcpPacket for adding additional funcitonality
+#[derive(Debug)]
+pub struct TcpSegment<'a>(pnet_TcpPacket<'a>);
+
+impl<'a> From<pnet_TcpPacket<'a>> for TcpSegment<'a> {
+    fn from(ipv4_packet: pnet_TcpPacket<'a>) -> Self {
+        TcpSegment(ipv4_packet)
+    }
+}
+
+impl<'a> Deref for TcpSegment<'a> {
+    type Target = pnet_TcpPacket<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl TcpSegment<'_> {
+    /// Return true if the TCP segment has a payload
+    pub fn has_payload(&self) -> bool {
+        !&self.payload().is_empty()
+    }
+
+    pub fn create_clone<'a>(&self) -> TcpSegment<'a> {
+        TcpSegment::from(pnet_TcpPacket::owned(self.packet().to_vec()).unwrap())
+    }
+
+    fn is_answered_by(&self, other: &TcpSegment<'_>) -> bool {
+        self.get_source() == other.get_destination()
+            && self.get_destination() == other.get_source()
+            && self.get_sequence() as usize + self.payload().len()
+                == other.get_acknowledgement() as usize
+    }
+}
+
+/// Wrapper around an Arc<[TcpSegment]> for additional functionality
+#[derive(Debug)]
+pub struct TcpSegmentCollection<'a>(Arc<[TcpSegment<'a>]>);
+
+impl<'a> FromIterator<TcpSegment<'a>> for TcpSegmentCollection<'a> {
+    fn from_iter<I: IntoIterator<Item = TcpSegment<'a>>>(iter: I) -> Self {
+        TcpSegmentCollection(iter.into_iter().collect())
+    }
+}
+
+impl<'a> Deref for TcpSegmentCollection<'a> {
+    type Target = Arc<[TcpSegment<'a>]>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a> From<crate::Ipv4PacketCollection<'a>> for TcpSegmentCollection<'a> {
+    fn from(ipv4_packet_collection: crate::Ipv4PacketCollection) -> Self {
+        ipv4_packet_collection
+            .iter()
+            .filter(|ipv4_packet| pnet_TcpPacket::new(ipv4_packet.payload()).is_some())
+            .map(|ipv4_packet| {
+                TcpSegment::from(pnet_TcpPacket::owned(ipv4_packet.payload().to_vec()).unwrap())
+            })
+            .collect::<TcpSegmentCollection>()
+    }
+}
+
+impl<'a> TcpSegmentCollection<'a> {
+    /// Get a collection of TcpSegment with TCP payloads
+    ///
+    /// Returns a new TcpSegmentCollection containing only the segments that have a TCP payload
+    pub fn filter_no_payload(&'a self) -> TcpSegmentCollection<'a> {
+        TcpSegmentCollection(
+            self.iter()
+                .filter(|s| s.has_payload())
+                .map(|s| s.create_clone())
+                .collect::<Arc<[TcpSegment]>>(),
+        )
+    }
+
+    /// Couple the challenge / response pairs in a collection of TCP segments
+    ///
+    /// Returns a new TcpSegmentCollection containing only the segments that have a TCP payload
+    pub fn find_challenge_response_pairs(
+        &'a mut self,
+    ) -> (TcpChallengeResponseCollection<'a>, TcpSegmentCollection<'a>) {
+        let mut matched = Vec::new();
+        let mut unmatched = self
+            .iter()
+            .map(|s| s.create_clone())
+            .collect::<Vec<TcpSegment<'a>>>();
+        let mut i = 0;
+        while i < unmatched.len() {
+            let challenge = unmatched[i].create_clone();
+            let mut j = 0;
+            let mut found_match = false;
+            while j < unmatched.len() - 1 {
+                j += 1;
+                let candidate = unmatched[j].create_clone();
+                if challenge.is_answered_by(&candidate) {
+                    matched.push(TcpChallengeResponse::new(
+                        challenge.create_clone(),
+                        candidate.create_clone(),
+                    ));
+                    unmatched.remove(i);
+                    unmatched.remove(j);
+                    found_match = true;
+                    break;
+                }
+            }
+            if !found_match {
+                i += 1;
+            }
+        }
+        (
+            TcpChallengeResponseCollection(matched.into()),
+            TcpSegmentCollection(unmatched.into()),
+        )
+    }
+}
+
+/// Container for TCP segments where the "challenge" was answered by the "response"
+#[derive(Debug)]
+pub struct TcpChallengeResponse<'a> {
+    pub challenge: TcpSegment<'a>,
+    pub response: TcpSegment<'a>,
+}
+
+impl<'a> TcpChallengeResponse<'a> {
+    fn new(challenge: TcpSegment<'a>, response: TcpSegment<'a>) -> TcpChallengeResponse<'a> {
+        TcpChallengeResponse {
+            challenge,
+            response,
+        }
+    }
+}
+
+/// Wrapper around an Arc<[TcpChallengeResponse]> for additional functionality
+#[derive(Debug)]
+pub struct TcpChallengeResponseCollection<'a>(Arc<[TcpChallengeResponse<'a>]>);
+
+impl<'a> FromIterator<TcpChallengeResponse<'a>> for TcpChallengeResponseCollection<'a> {
+    fn from_iter<I: IntoIterator<Item = TcpChallengeResponse<'a>>>(iter: I) -> Self {
+        TcpChallengeResponseCollection(iter.into_iter().collect())
+    }
+}
+
+impl<'a> Deref for TcpChallengeResponseCollection<'a> {
+    type Target = Arc<[TcpChallengeResponse<'a>]>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a> DerefMut for TcpChallengeResponseCollection<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
